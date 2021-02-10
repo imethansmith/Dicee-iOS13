053a33a5aef62cf9d462812408f537098910f754
Ethan Smith <edhsmith@icloud.com>
2021-02-10 14:58:02 +1300

    Simplified and optimised code. Final edits for basic app completion.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,18 +13,15 @@ class ViewController: UIViewController {
     // Main UI elements connected to Storyboard
     @IBOutlet weak var diceImageViewOne: UIImageView!
     @IBOutlet weak var diceImageViewTwo: UIImageView!
-    
-    var leftDiceNumber = 1
-    var rightDiceNumber = 2
+
     let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
     // Roll button setup
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        leftDiceNumber = Int.random(in: 0...5)
-        rightDiceNumber = Int.random(in: 0...5)
 
-        diceImageViewOne.image = diceArray[leftDiceNumber]
-        diceImageViewTwo.image = diceArray[rightDiceNumber]
+        // Randomised array elemental retrieval on button press
+        diceImageViewOne.image = diceArray.randomElement()
+        diceImageViewTwo.image = diceArray.randomElement()
     }
     
 }
